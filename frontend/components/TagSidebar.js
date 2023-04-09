16637eb9690cae0cadf2d142e94080761fd21e65
MetalPower26 <MetalPower.github@gmail.com>
2023-04-09 20:12:50 +0700

    Fix sidebar

--- frontend/components/TagSidebar.js
+++ frontend/components/TagSidebar.js
@@ -1,26 +1,54 @@
 import {tags, languages} from "../app/info";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function TagSidebar () {
+  const router = useRouter();
+  const { filter } = router.query;
+
+  let AllComponent = (<input type="checkbox" className="form-checkbox" />)
+  if (filter === undefined) {
+    AllComponent = <input type="checkbox" className="form-checkbox" checked />
+  }
+
   return (
-    <div className="flex flex-col bg-white rounded-md h-full w-full lg:w-72 lg:border-r lg:pr-4">
+    <div className="flex flex-col bg-white rounded-md h-full w-72 p-2 lg:border-r">
       {/* Filter title */}
-      <h2 className="font-medium text-lg py-4 px-2 lg:px-4">Filter</h2>
+      <h2 className="font-medium text-lg py-4 px-4">Filter</h2>
 
       {/* Filter options */}
-      <div className="flex-1 overflow-y-auto py-4 px-2 lg:px-4">
+      <div className="flex-1 overflow-y-auto py-4 px-4">
         {/* Option group */}
         <div className="mb-4">
           <h3 className="font-medium text-sm mb-2">Tags</h3>
           <ul>
-            {tags.map((item, index) => (
-              <li key={index}>
-                <label className="inline-flex items-center">
-                  <input type="checkbox" className="form-checkbox" />
-                  <span className="ml-2">{item}</span>
-                </label>
-              </li>
-            ))}
+            {
+              tags.map((item, index) => {
+                if (filter && item === filter) {
+                  return (
+                    <li key={index}>
+                      <Link href={`../projects?filter=${encodeURIComponent(item)}`}>
+                        <label className="inline-flex items-center">
+                          <input type="checkbox" className="form-checkbox" checked />
+                          <span className="ml-2">{item}</span>
+                        </label>
+                      </Link>
+                    </li>
+                  );
+                } else {
+                  return (
+                    <li key={index}>
+                      <Link href={`../projects?filter=${encodeURIComponent(item)}`}>
+                        <label className="inline-flex items-center">
+                          <input type="checkbox" className="form-checkbox" />
+                          <span className="ml-2">{item}</span>
+                        </label>
+                      </Link>
+                    </li>
+                  );
+                }
+              }
+            )}
             {/* more options... */}
           </ul>
         </div>
@@ -29,16 +57,42 @@ export default function TagSidebar () {
         <div className="mb-4">
           <h3 className="font-medium text-sm mb-2">Languages</h3>
           <ul>
-            {languages.map((item, index) => (
-              <li key={index}>
-                <label className="inline-flex items-center">
-                  <input type="checkbox" className="form-checkbox" />
-                  <span className="ml-2">{item}</span>
-                </label>
-              </li>
-            ))}
+            {languages.map((item, index) => {
+              if (filter && item === filter) {
+                return (
+                  <li key={index}>
+                    <Link href={`../projects?filter=${encodeURIComponent(item)}`}>
+                      <label className="inline-flex items-center">
+                        <input type="checkbox" className="form-checkbox" checked />
+                        <span className="ml-2">{item}</span>
+                      </label>
+                    </Link>
+                  </li>
+                );
+              } else {
+                return (
+                  <li key={index}>
+                    <Link href={`../projects?filter=${encodeURIComponent(item)}`}>
+                      <label className="inline-flex items-center">
+                        <input type="checkbox" className="form-checkbox" />
+                        <span className="ml-2">{item}</span>
+                      </label>
+                    </Link>
+                  </li>
+                );
+              }
+            })}
             {/* more options... */}
           </ul>
+        </div>
+
+        <div className="mb-4">
+          <Link href={`../projects`}>
+            <label className="inline-flex items-center">
+              {AllComponent}
+              <span className="ml-2">All</span>
+            </label>
+          </Link>
         </div>
       </div>
     </div>
--- frontend/pages/projects/index.tsx
+++ frontend/pages/projects/index.tsx
@@ -1,16 +1,24 @@
 import Navbar from "../../components/Navbar"
 import TagSidebar from "../../components/TagSidebar"
 
+function Content () {
+  return (
+    <div className="flex flex-col bg-white rounded-md h-full w-full p-2 lg:border-r">
+      <h1 className="font-2xl"> Content </h1>
+    </div>
+  )
+}
+
 export default function Projects () {
   return (
     <main className="flex bg-gray-200 min-h-screen flex-col place-items-center">
       <Navbar />
-      <div className="flex flex-row my-12 w-4/5 lg:w-11/12 h-100 justify-items-start">
-        <div className="flex mr-12 w-52">
+      <div className="flex flex-row my-6 lg:my-12 w-11/12 h-100 justify-items-start">
+        <div className="flex mr-6 lg:mr-12 w-40">
           <TagSidebar />
         </div>
-        <div className="flex w-72">
-          <h1> Content </h1>
+        <div className="flex grow">
+          <Content />
         </div>
       </div>
     </main>
