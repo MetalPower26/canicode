438de5d486fc8ee5b79f43ce9effeccee87a0ee4
MetalPower26 <MetalPower.github@gmail.com>
2023-07-13 14:28:22 +0700

    Firebase and Auth

--- frontend/firebase/firebase.tsx
+++ frontend/firebase/firebase.tsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,6 +19,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default firebase;
--- frontend/pages/auth.tsx
+++ frontend/pages/auth.tsx
@@ -1,35 +1,29 @@
 import React from "react";
-import firebase from "@/firebase/firebase";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import firebase from "../firebase/firebase";
 
 import Image from 'next/image';
 import Navbar from "@/components/Navbar";
 
+const auth = firebase.auth();
+const GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
 export default function Login() {
 
-  function handleSignIn(){
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        if(credential === null) throw 'Credential is null';
-        const token = credential.accessToken;
-        const user = result.user;
-        console.log(String(user) + ' has signed in');
-      }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log('Error ' + String(errorMessage));
-      });
-  }
+  const signInWithGoogle = async () => {
+    try {
+        await auth.signInWithPopup(GoogleAuthProvider);
+        console.log("Success");
+    } catch (err) {
+        Error("Failed to log in");
+    }
+  };
   
   return (
     <main className="flex min-h-screen flex-col items-center">
       <Navbar />
       <div className="flex flex-col items-center justify-center h-screen">
         <h2 className="text-2xl font-bold mb-4">Sign in with Google</h2>
-        <button onClick={ handleSignIn } className="flex items-center px-4 py-2 bg-red-600 text-white rounded-md shadow-md hover:bg-red-700">
+        <button onClick={ signInWithGoogle } className="flex items-center px-4 py-2 bg-red-600 text-white rounded-md shadow-md hover:bg-red-700">
           <Image
             src="../public/google-logo.png"
             alt="Google Logo"
