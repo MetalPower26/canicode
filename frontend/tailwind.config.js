a62d5c40dbd3a487af61063cb037207b69c3dfdf
MetalPower26 <MetalPower.github@gmail.com>
2023-07-16 16:34:26 +0700

    Update styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,15 +12,23 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'blue': {
+          500: '#164B60',
+          400: '#1B6B93',
+          300: '#3596B2',
+          200: '#4FC0D0',
+          100: '#B9E6EC',
+        },
+        'teal': {
+          light:'#D5F3F0',
+          DEFAULT:'#2EC4B6',
+          dark: '#219186',
+        },
+        'orange': '#FF4A1C',
+        'coffee': '#81523F',
+      },
     },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'blue': '#226CE0',
-      'teal': '#2EC4B6',
-      'orange': '#FF4A1C',
-      'coffee': '#81523F',
-    }
   },
   plugins: [],
 }
