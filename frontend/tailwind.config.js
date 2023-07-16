4fad462a752a17af6d93f9c18b62382e26f528d9
MetalPower26 <MetalPower.github@gmail.com>
2023-07-16 15:32:27 +0700

    Add custom style

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,6 +13,14 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'blue': '#226CE0',
+      'teal': '#2EC4B6',
+      'orange': '#FF4A1C',
+      'coffee': '#81523F',
+    }
   },
   plugins: [],
 }
