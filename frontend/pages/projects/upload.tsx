ae0412d094ab9a473709ec489358571786b12e6f
MetalPower26 <MetalPower.github@gmail.com>
2023-07-16 17:39:29 +0700

    Add placeholder to pages

---
+++ frontend/pages/projects/upload.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar"
+
+export default function Upload() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <Navbar />
+      <div className="mt-5">
+        <h1 className="text-2xl"> Upload </h1>
+      </div>
+    </main>
+  )
+}
