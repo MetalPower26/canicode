44d6cb3aeb15456d24f13d9288aeeaa9399fec02
MetalPower26 <MetalPower.github@gmail.com>
2023-07-16 15:11:36 +0700

    Remove users functions

---
+++ frontend/pages/leaderboard.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+export default function Leaderboard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <Navbar />
+      <div className="mt-5">
+        <h1 className="text-2xl"> Leaderboard </h1>
+      </div>
+    </main>
+  )
+}
--- frontend/users/users.tsx
+++ frontend/users/users.tsx
@@ -1,26 +1,11 @@
 import { app, auth } from "@/firebase/firebase";
 import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
-// isAuthenticated returns a boolean value to check if the user is logged in or not
-
-const isAuthenticated = () => {
-  const user = auth.currentUser;
-  return user !== null;
-}
-
-// getUser returns a auth.User object if signed in or null if not
-// Please always use isAuthenticated first to check, or remember to handle null
-
-const getUser = () => {
-  const user = auth.currentUser;
-  return user;
-}
-
-// Async function GoogleSignIn starts PopUp Sign in process
 // Returns a auth.User object of the user if signed in or null if signed in process failed
 
 const GoogleSignIn = async () => {
   const provider = new GoogleAuthProvider();
+
   try {
     const result = await signInWithPopup(auth, provider);
     const user = result.user;
@@ -47,4 +32,4 @@ const userSignOut = async () => {
   }
 }
 
-export { isAuthenticated, getUser, GoogleSignIn, userSignOut }
+export { GoogleSignIn, userSignOut }
